4282717d43ae99642a0d450ffb7642e8c6924c0a
Usamaliaquat123 <inconnent12345@outlook.com>
2021-01-03 01:44:22 -0800

    rep @comp

--- src/containers/home/SOR/ViewAll/styles.ts
+++ src/containers/home/SOR/ViewAll/styles.ts
@@ -1,5 +1,145 @@
 import {StyleSheet} from 'react-native';
+import {colors} from '@theme';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+const styles = StyleSheet.create({
+  header: {
+    padding: wp(5),
+    paddingRight: wp(7),
+    paddingLeft: wp(7),
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  title: {
+    fontSize: wp(4),
+    marginLeft: wp(5),
+    color: colors.secondary,
+    fontWeight: 'bold',
+  },
+  underScrore: {
+    backgroundColor: colors.green,
+    width: wp(6),
+    marginLeft: wp(5),
+    padding: wp(0.5),
+    borderRadius: 10,
+  },
+  headertle: {flexDirection: 'row'},
+  headerSelect: {
+    marginTop: wp(10),
+    flexDirection: 'row',
+    // alignContent: 'stretch',
+    justifyContent: 'space-between',
+    width: wp(100),
+  },
 
-const styles = StyleSheet.create({});
+  hselectort: {color: colors.secondary, fontWeight: 'bold', fontSize: wp(3)},
+  selectorBox: {color: colors.secondary, fontSize: wp(3)},
+  rightSelector: {flexDirection: 'row', marginRight: wp(12)},
+  leftSelector: {flexDirection: 'row'},
+  selector: {
+    paddingLeft: 3,
+    width: wp(20),
+  },
+
+  slctContainer: {
+    position: 'absolute',
+    top: wp(5),
+    zIndex: 1,
+    left: wp(13),
+    backgroundColor: colors.secondary,
+    // padding: wp(5),
+    paddingLeft: wp(2),
+    paddingRight: wp(2),
+    paddingTop: wp(1),
+    paddingBottom: wp(1),
+    width: wp(22),
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+
+  itemH: {
+    color: 'black',
+    fontSize: wp(3),
+    padding: wp(1),
+  },
+  avatarView: {
+    position: 'absolute',
+    right: 0,
+  },
+
+  // content
+  cardContainer: {
+    width: wp(70),
+    borderRadius: wp(3),
+    padding: wp(5),
+    // borderWidth: wp(0.1),
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    marginBottom: wp(10),
+    elevation: 1,
+  },
+  cardHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardtime: {
+    fontSize: wp(3),
+    fontWeight: 'bold',
+    color: colors.text,
+  },
+  cardDate: {
+    fontSize: wp(3),
+    color: colors.text,
+    fontWeight: 'bold',
+  },
+  cardbadge: {
+    backgroundColor: colors.riskIcons.yellow,
+    padding: wp(5),
+    marginRight: wp(1),
+    justifyContent: 'center',
+    marginTop: wp(-2),
+    alignSelf: 'center',
+    borderRadius: wp(10),
+  },
+  cardBadgeText: {
+    fontSize: wp(3),
+    fontWeight: 'bold',
+    position: 'absolute',
+    alignSelf: 'center',
+  },
+  cardTitle: {
+    fontSize: wp(4),
+    color: colors.primary,
+    // fontWeight: 'bold',
+  },
+  cardBottom: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  cardLocation: {
+    flexDirection: 'row',
+  },
+  cardBorderText: {
+    fontSize: wp(3),
+    color: colors.text,
+  },
+  cardRisk: {
+    flexDirection: 'row',
+  },
+});
 
 export default styles;
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
 import {Icon, Avatar} from 'react-native-elements';
 import {colors} from '@theme';
-import {StackNavigationProp} from '@react-navigation/stack';
 import {Create_sor, View_sor, all_sor, viewas} from '@service';
 import {connect} from 'react-redux';
 import * as reduxActions from '../../store/Actions/AppActions';
+import {StackNavigationProp} from '@react-navigation/stack';
 import {StackNavigatorProps} from '@nav';
 import {RouteProp} from '@react-navigation/native';
 import {
@@ -32,153 +32,13 @@ export interface HomeProps {
 class Home extends React.Component<HomeProps, any> {
   constructor(props: any) {
     super(props);
-    this.state = {
-      currentlocation: Create_sor.Observation.locations[0],
-      project: viewas[0],
-      selectP: false,
-    };
-    // console.log(this.props.reduxState);
+    this.state = {};
   }
 
   render() {
     return (
       <View style={{flex: 1, backgroundColor: colors.primary}}>
-        <ScrollView>
-          <View style={styles.header}>
-            <View style={styles.headertle}>
-              <Icon
-                size={25}
-                name="arrow-back-outline"
-                type="ionicon"
-                color={colors.secondary}
-              />
-              <View>
-                <Text style={styles.title}>View SOR Reports</Text>
-                <View style={styles.underScrore} />
-              </View>
-              <View style={styles.avatarView}>
-                <Avatar
-                  rounded
-                  source={{
-                    uri: Create_sor.user.profile,
-                  }}
-                />
-              </View>
-            </View>
-            <View style={styles.headerSelect}>
-              {/* Project selector */}
-              <View style={styles.leftSelector}>
-                {/* <Text style={styles.hselectort}> Board View</Text> */}
-                <TouchableOpacity
-                  style={styles.selector}
-                  // onPress={() => {
-                  //   this.setState({selectP: !this.state.selectP});
-                  // }}>
-                >
-                  <Text style={styles.selectorBox}>{this.state.project}</Text>
-                </TouchableOpacity>
-                <Icon
-                  style={{padding: 3}}
-                  size={10}
-                  name="down"
-                  type="antdesign"
-                  color={colors.secondary}
-                />
-                {this.state.selectP == true ? (
-                  <View style={styles.slctContainer}>
-                    {Create_sor.Observation.projects.map((d, i) => (
-                      <Text
-                        key={i}
-                        onPress={() => this.setState({project: d})}
-                        style={styles.itemH}>
-                        {d.length > 7 ? d.substring(0, 8) + '...' : d}
-                      </Text>
-                    ))}
-                  </View>
-                ) : null}
-              </View>
-            </View>
-          </View>
-          <View
-            style={{
-              flex: 2,
-              backgroundColor: colors.secondary,
-              borderTopLeftRadius: wp(10),
-              borderTopRightRadius: wp(10),
-              padding: wp(10),
-              height: wp(150),
-            }}>
-            <Icon
-              onPress={() => this.props.navigation.navigate('CreateSOR')}
-              style={{padding: 3}}
-              size={wp(15)}
-              name="file-certificate-outline"
-              type="material-community"
-              color={colors.primary}
-            />
-            {/* <Chart
-              style={{alignSelf: 'center', marginTop: wp(3)}}
-              onPress={(v: number) => console.log(v)}
-            /> */}
-            {/* <ScrollView horizontal={true}>
-              <ScrollView>
-                <Text>sdsds</Text>
-              </ScrollView>
-              <ScrollView style={{marginRight: wp(10), marginLeft: wp(10)}}>
-                <View style={styles.cardContainer}>
-                  <View style={styles.cardHeader}>
-                    <Text style={styles.cardtime}>123</Text>
-                    <TouchableOpacity style={styles.cardbadge}>
-                      <Text style={styles.cardBadgeText}>4</Text>
-                    </TouchableOpacity>
-                    <Text style={styles.cardDate}>22/10/2020</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.cardTitle}>
-                      Consequat Lorem qui excepteur pariatur laborum amet
-                      officia incididunt dolore.
-                    </Text>
-                  </View>
-                  <View style={styles.cardBottom}>
-                    <View style={styles.cardRisk}>
-                      <Icon
-                        style={{padding: 3}}
-                        size={wp(3)}
-                        name="warning"
-                        type="antdesign"
-                        color={colors.riskIcons.red}
-                      />
-                      <Text
-                        style={[
-                          styles.cardBorderText,
-                          {color: colors.riskIcons.red},
-                        ]}>
-                        Concern
-                      </Text>
-                    </View>
-                    <View style={styles.cardLocation}>
-                      <Icon
-                        style={{padding: 3}}
-                        size={15}
-                        name="location"
-                        type="evilicon"
-                        color={colors.primary}
-                      />
-                      <Text style={styles.cardBorderText}>Lorem ipsum </Text>
-                    </View>
-                  </View>
-                </View>
-              </ScrollView>
-              <ScrollView>
-                <Text>sdsd</Text>
-                <Text>sdsd</Text>
-                <Text>sdsd</Text>
-                <Text>sdsd</Text>
-                <Text>sdsd</Text>
-              </ScrollView> */}
-            {/* </ScrollView> */}
-          </View>
-        </ScrollView>
+        <ScrollView></ScrollView>
       </View>
     );
   }
