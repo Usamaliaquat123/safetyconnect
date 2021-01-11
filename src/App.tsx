4a2e348978ca8b37160ab4096bb5cd95746e2e58
Usamaliaquat123 <inconnent12345@outlook.com>
2021-01-11 18:11:13 +0500

    trigger sentry commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import {View, StyleSheet, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import Store from './store/store';
 import {MainStackNavigator} from '@nav';
+import { sentryConf} from "@config";
+import * as Sentry from "@sentry/react-native";
+
 export interface AppProps {}
 
 export default class App extends React.Component<AppProps, any> {
+  
+  componentDidMount = () => {
+    Sentry.init({
+      dsn: "https://67f8a7e80a9c4b2a9579b6b2cf191312@o503166.ingest.sentry.io/5587869",
+    });
+  
+  }
   render() {
+
     return (
       <Provider store={Store}>
         <MainStackNavigator />
