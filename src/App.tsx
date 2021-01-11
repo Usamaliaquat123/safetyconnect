5c30441daa5e4f6d5d0cc288e98b0860fb7ee7be
Usamaliaquat123 <inconnent12345@outlook.com>
2021-01-11 19:35:35 +0500

    conf

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import {View, StyleSheet, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import Store from './store/store';
 import {MainStackNavigator} from '@nav';
-import * as Sentry from "@sentry/react-native";
+import { configSentry } from "config";
 
 export interface AppProps {}
 
 export default class App extends React.Component<AppProps, any> {
   
   componentDidMount = () => {
-    Sentry.init({
-      dsn: "https://67f8a7e80a9c4b2a9579b6b2cf191312@o503166.ingest.sentry.io/5587869",
-    });
-  
+    configSentry().catch(err => new Error(`Error when configure sentry ${err}`))
   }
   render() {
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,20 @@
 
 
+import * as Sentry from "@sentry/react-native";
 
-export default  {};
+// configure Sentry
+const configSentry = () => {
+    return new Promise((resolve, reject) => {
+    try {
+        Sentry.init({
+            dsn: "https://67f8a7e80a9c4b2a9579b6b2cf191312@o503166.ingest.sentry.io/5587869",
+          });
+          resolve(true)
+    } catch (error) {
+        reject(error)
+    }        
+    })
+}
+
+
+export  {configSentry};
