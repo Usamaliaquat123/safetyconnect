1e787039a0a91b9a5eb367715c48b9a401428dee
Usamaliaquat123 <inconnent12345@outlook.com>
2021-02-11 23:00:49 -0800

    add loading

--- src/containers/auth/login/Login.tsx
+++ src/containers/auth/login/Login.tsx
@@ -40,6 +40,7 @@ class Login extends React.Component<LoginProps, any> {
       selectedInput: 1,
       isEye: true,
       username: '',
+      loading: false,
       password: '',
     };
   }
@@ -47,6 +48,7 @@ class Login extends React.Component<LoginProps, any> {
   submitSignin = async (e: string) => {
     // event.preventDefault();
     // this.props.navigation.navigate('sor');
+    this.setState({loading: true});
     const attr = {
       profile: 'NotConfirmed',
     };
@@ -61,11 +63,14 @@ class Login extends React.Component<LoginProps, any> {
 
       if (user.userConfirmed) {
         const sendEmail = await Auth.forgotPassword(e);
+        this.setState({loading: false});
         if (sendEmail) this.props.navigation.navigate('Verify');
       } else {
+        this.setState({loading: false});
         this.props.navigation.navigate('sor');
       }
     } catch (err) {
+      this.setState({loading: false});
       console.log(err);
     }
   };
@@ -82,98 +87,134 @@ class Login extends React.Component<LoginProps, any> {
             </View>
           </View>
           {/* content */}
-          <View style={styles.content}>
-            <Text style={styles.headingContainer}>Sign In</Text>
-            {/* inputs container */}
-            <View style={styles.inputsContainer}>
-              {/* Email Container */}
-              <Text style={styles.emailTextContainer}>Email</Text>
+          <View style={[styles.content]}>
+            {this.state.loading == true ? (
               <View
-                style={[
-                  styles.inputContainer,
-                  this.state.selectedInput == 1
-                    ? {borderColor: colors.green}
-                    : {borderColor: colors.textOpa},
-                ]}>
-                <TextInput
-                  onFocus={() => this.setState({selectedInput: 1})}
-                  style={styles.authInputs}
-                  onChange={(e) =>
-                    this.setState({username: e.nativeEvent.text})
-                  }
-                  value={this.state.username}
-                  placeholder={'Enter your email'}
+                style={{
+                  alignSelf: 'center',
+                  marginTop: wp(40),
+                }}>
+                <LottieView
+                  // ref={(animation) => {
+                  //   this.photoAnim = animation;
+                  // }}
+                  autoPlay={true}
+                  style={{width: wp(70)}}
+                  source={animation.loading}
+                  loop={true}
                 />
+                <Text
+                  style={{
+                    fontSize: wp(3.5),
+                    opacity: 0.5,
+                    textAlign: 'center',
+                    marginTop: wp(5),
+                  }}>
+                  Connecting...
+                </Text>
               </View>
-              {/* Password Container */}
+            ) : (
+              <View>
+                <Text style={styles.headingContainer}>Sign In</Text>
+                {/* inputs container */}
+                <View style={styles.inputsContainer}>
+                  {/* Email Container */}
+                  <Text style={styles.emailTextContainer}>Email</Text>
+                  <View
+                    style={[
+                      styles.inputContainer,
+                      this.state.selectedInput == 1
+                        ? {borderColor: colors.green}
+                        : {borderColor: colors.textOpa},
+                    ]}>
+                    <TextInput
+                      onFocus={() => this.setState({selectedInput: 1})}
+                      style={styles.authInputs}
+                      onChange={(e) =>
+                        this.setState({username: e.nativeEvent.text})
+                      }
+                      value={this.state.username}
+                      placeholder={'Enter your email'}
+                    />
+                  </View>
+                  {/* Password Container */}
 
-              <Text style={styles.passTextContainer}>Password</Text>
-              <View
-                style={[
-                  styles.inputContainer,
-                  this.state.selectedInput == 2
-                    ? {borderColor: colors.green}
-                    : {borderColor: colors.textOpa},
-                ]}>
-                <TextInput
-                  secureTextEntry={this.state.isEye}
-                  onFocus={() => this.setState({selectedInput: 2})}
-                  style={styles.authInputs}
-                  value={this.state.password}
-                  onChange={(e) =>
-                    this.setState({password: e.nativeEvent.text})
-                  }
-                  placeholder={'******'}
-                />
+                  <Text style={styles.passTextContainer}>Password</Text>
+                  <View
+                    style={[
+                      styles.inputContainer,
+                      this.state.selectedInput == 2
+                        ? {borderColor: colors.green}
+                        : {borderColor: colors.textOpa},
+                    ]}>
+                    <TextInput
+                      secureTextEntry={this.state.isEye}
+                      onFocus={() => this.setState({selectedInput: 2})}
+                      style={styles.authInputs}
+                      value={this.state.password}
+                      onChange={(e) =>
+                        this.setState({password: e.nativeEvent.text})
+                      }
+                      placeholder={'******'}
+                    />
+                    <TouchableOpacity
+                      onPress={() => this.setState({isEye: !this.state.isEye})}
+                      style={styles.eyeIconContainer}>
+                      {this.state.isEye == true ? (
+                        <Icon
+                          containerStyle={{opacity: 0.5}}
+                          size={wp(5)}
+                          name="eye-with-line"
+                          type="entypo"
+                          color={colors.text}
+                        />
+                      ) : (
+                        <Icon
+                          containerStyle={{opacity: 0.5}}
+                          size={wp(5)}
+                          name="eye"
+                          type="antdesign"
+                          color={colors.text}
+                        />
+                      )}
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <Text style={styles.forgetPassText}>Forget Password ? </Text>
                 <TouchableOpacity
-                  onPress={() => this.setState({isEye: !this.state.isEye})}
-                  style={styles.eyeIconContainer}>
-                  {this.state.isEye == true ? (
-                    <Icon
-                      containerStyle={{opacity: 0.5}}
-                      size={wp(5)}
-                      name="eye-with-line"
-                      type="entypo"
-                      color={colors.text}
-                    />
-                  ) : (
-                    <Icon
-                      containerStyle={{opacity: 0.5}}
-                      size={wp(5)}
-                      name="eye"
-                      type="antdesign"
-                      color={colors.text}
-                    />
-                  )}
+                  onPress={() => this.submitSignin(this.state.username)}
+                  style={styles.siginBtnContainer}>
+                  <Text style={styles.signinText}>Sign in </Text>
+                </TouchableOpacity>
+                {/* Or */}
+                <View style={styles.orContainer}>
+                  <View style={styles.line} />
+                  <Text style={styles.orText}>OR</Text>
+                  <View style={styles.line} />
+                </View>
+                {/* Google Signin */}
+                <TouchableOpacity style={styles.siginwithGoogle}>
+                  <View
+                    style={{width: wp(7), height: wp(7), marginRight: wp(3)}}>
+                    <Image source={images.google} style={GlStyles.images} />
+                  </View>
+                  <Text style={styles.signinTextGoogle}>
+                    Continue with Google{' '}
+                  </Text>
+                </TouchableOpacity>
+                {/* Don't have a Acctouny */}
+                <Text style={styles.dontHaveAccount}>
+                  Don't have an Account ?
+                </Text>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate('Signup')}
+                  style={styles.createnewaccountContainer}>
+                  <Text style={styles.createNewAccount}>
+                    Create New Account
+                  </Text>
                 </TouchableOpacity>
               </View>
-            </View>
-            <Text style={styles.forgetPassText}>Forget Password ? </Text>
-            <TouchableOpacity
-              onPress={() => this.submitSignin(this.state.username)}
-              style={styles.siginBtnContainer}>
-              <Text style={styles.signinText}>Sign in </Text>
-            </TouchableOpacity>
-            {/* Or */}
-            <View style={styles.orContainer}>
-              <View style={styles.line} />
-              <Text style={styles.orText}>OR</Text>
-              <View style={styles.line} />
-            </View>
-            {/* Google Signin */}
-            <TouchableOpacity style={styles.siginwithGoogle}>
-              <View style={{width: wp(7), height: wp(7), marginRight: wp(3)}}>
-                <Image source={images.google} style={GlStyles.images} />
-              </View>
-              <Text style={styles.signinTextGoogle}>Continue with Google </Text>
-            </TouchableOpacity>
-            {/* Don't have a Acctouny */}
-            <Text style={styles.dontHaveAccount}>Don't have an Account ?</Text>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('Signup')}
-              style={styles.createnewaccountContainer}>
-              <Text style={styles.createNewAccount}>Create New Account</Text>
-            </TouchableOpacity>
+            )}
           </View>
         </ScrollView>
         {/* <Model
@@ -183,14 +224,7 @@ class Login extends React.Component<LoginProps, any> {
           animationOutTiming={2000}
           isVisible={true}
           onBackdropPress={() => this.setState({addAssigners: false})}>
-          <LottieView
-            ref={(animation) => {
-              this.photoAnim = animation;
-            }}
-            style={{width: wp(70)}}
-            source={animation.loading}
-            loop={true}
-          />
+        
         </Model> */}
       </View>
     );
