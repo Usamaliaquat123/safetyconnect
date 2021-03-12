a398c3ec25b2ffd89a5958c255acc352d32cdaf4
Usamaliaquat123 <inconnent12345@outlook.com>
2021-03-12 11:30:12 +0500

    working on create popup

---
+++ src/components/BottomTab/styles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+import {colors} from '@theme';
+const styles = StyleSheet.create({
+  addNewContainer: {
+    position: 'absolute',
+    zIndex: wp(2),
+    top: wp(-7),
+    backgroundColor: colors.primary,
+    borderRadius: wp(10),
+    padding: wp(4),
+  },
+  addNewText: {
+    marginLeft: wp(2.2),
+    marginTop: wp(10),
+    fontSize: wp(3),
+  },
+  containerOfIcon: {
+    marginTop: wp(1),
+    // justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  auditReportText: {
+    paddingLeft: wp(2),
+    fontSize: wp(3),
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default styles;
