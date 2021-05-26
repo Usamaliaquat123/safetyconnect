bef6cc7c8a1a251276ef5485f2f13978697b7a28
Usamaliaquat123 <inconnent12345@outlook.com>
2021-05-26 10:28:05 +0500

    meet before implemented

--- src/containers/auth/MeetBefore/MeetBefore.tsx
+++ src/containers/auth/MeetBefore/MeetBefore.tsx
@@ -12,9 +12,14 @@ import {connect} from 'react-redux';
 import {RouteProp, CommonActions} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {StackNavigatorProps} from '@nav';
+import {validatePassword} from '@utils';
 import styles from './styles';
 import {colors, images, GlStyles, fonts} from '@theme';
 import {Icon} from 'react-native-elements';
+import {Auth} from 'aws-amplify';
+import {Create_sor, createApi} from '@service';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -37,13 +42,57 @@ class MeetBefore extends React.Component<MeetBeforeProps, any> {
     super(props);
     this.state = {
       email: 'inconnent12345@outlook.com', //this.props.route.params.email
+      password: '',
+      passError: false,
     };
   }
 
   componentDidMount() {}
 
-  continueLogin = () => {};
+  submitSignin = async () => {
+    if (
+      this.state.password != '' &&
+      validatePassword(this.state.password) == true
+    ) {
+      this.setState({passError: false});
+      try {
+        /*
+         * @Default email : asohial.bscs16seecs@seecs.edu.pk || password: Weird.password02
+         */
 
+        this.setState({loading: true, errorModal: true});
+        const user = await Auth.signIn(this.state.email, this.state.password);
+        this.setState({errorModal: false, loading: false});
+
+        createApi
+          .createApi()
+          .getUser(this.state.username)
+          .then((user: any) => {
+            AsyncStorage.setItem('user', JSON.stringify(user.data.data));
+          });
+
+        AsyncStorage.setItem('email', this.state.username);
+
+        this.props.navigation.navigate('Main');
+      } catch (err) {
+        this.setState({errorModal: true, loading: false});
+      }
+    } else {
+      this.setState({passError: true});
+    }
+  };
+  // Continue with google
+  loginWithGoogle = async () => {
+    try {
+      // GoogleSignin.signIn().then(res => {
+      //   console.log(res)
+      // }).catch(err => console.log(err))
+
+      const user = await Auth.federatedSignIn({provider: 'Google'});
+
+      this.props.navigation.navigate('Main');
+    } catch (e) {}
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -76,7 +125,15 @@ class MeetBefore extends React.Component<MeetBeforeProps, any> {
                   fontFamily: fonts.SFuiDisplayMedium,
                   opacity: 0.5,
                 }}>
-                This email address is allready registered
+                This email address is allready registered, Please
+              </Text>
+              <Text
+                style={{
+                  fontSize: wp(3.3),
+                  fontFamily: fonts.SFuiDisplayMedium,
+                  opacity: 0.5,
+                }}>
+                enter password to access your account
               </Text>
 
               {/* Password Container */}
@@ -88,7 +145,7 @@ class MeetBefore extends React.Component<MeetBeforeProps, any> {
                   style={[
                     styles.inputContainer,
                     this.state.selectedInput == 2
-                      ? {borderColor: colors.green}
+                      ? {borderColor: colors.primary}
                       : {borderColor: colors.textOpa},
                   ]}>
                   <TextInput
@@ -104,6 +161,7 @@ class MeetBefore extends React.Component<MeetBeforeProps, any> {
                     style={styles.authInputs}
                     value={this.state.password}
                     onChange={(e) => {
+                      this.setState({passError: false});
                       this.setState({password: e.nativeEvent.text});
                     }}
                     placeholder={'******'}
@@ -150,37 +208,64 @@ class MeetBefore extends React.Component<MeetBeforeProps, any> {
                 )}
               </View>
 
-              {/* Don't have a Acctouny */}
-              <View style={styles.dtHaveContainer}>
-                <Text style={styles.dontHaveAccount}>
-                  Looks like you already have an account.
-                </Text>
-                <Text style={styles.dontHaveAccount}>
-                  please{' '}
-                  <Text
-                    onPress={() => {
-                      this.props.navigation.dispatch(
-                        CommonActions.reset({
-                          index: 1,
-                          routes: [
-                            {
-                              name: 'Login',
-                            },
-                          ],
-                        }),
-                      );
-                    }}
-                    style={{
-                      fontSize: wp(3),
-                      color: colors.primary,
-                      fontWeight: 'bold',
-                    }}>
-                    Sign In
-                  </Text>{' '}
-                </Text>
+              <View style={{marginTop: wp(10)}}>
+                <TouchableOpacity
+                  onPress={() => this.submitSignin()}
+                  style={styles.siginBtnContainer}>
+                  <Text style={styles.signinText}>Continue </Text>
+
+                  <Icon
+                    containerStyle={{marginLeft: wp(3)}}
+                    size={wp(5)}
+                    name="arrowright"
+                    type="antdesign"
+                    color={colors.secondary}
+                  />
+                </TouchableOpacity>
               </View>
 
+              {/* Or */}
+              <View style={styles.orContainer}>
+                <View style={styles.line} />
+                <Text style={styles.orText}>OR</Text>
+                <View style={styles.line} />
+              </View>
+              {/* Google Signin */}
               <TouchableOpacity
+                onPress={() => {
+                  this.loginWithGoogle();
+                }}
+                style={styles.siginwithGoogle}>
+                <View style={{width: wp(7), height: wp(7), marginRight: wp(3)}}>
+                  <Image source={images.google} style={GlStyles.images} />
+                </View>
+                <Text style={styles.signinTextGoogle}>
+                  Continue with Google{' '}
+                </Text>
+              </TouchableOpacity>
+              {/* Apple Signin */}
+              <TouchableOpacity
+                onPress={() => {
+                  this.loginWithGoogle();
+                }}
+                style={styles.siginwithGoogle}>
+                <View
+                  style={{width: wp(10), height: wp(7), marginRight: wp(3)}}>
+                  {/* <Image source={images.} style={GlStyles.images} /> */}
+                  <Icon
+                    containerStyle={{marginLeft: wp(3)}}
+                    size={wp(5)}
+                    name="apple1"
+                    type="antdesign"
+                    color={colors.text}
+                  />
+                </View>
+                <Text style={styles.signinTextGoogle}>
+                  Continue with Apple ID{' '}
+                </Text>
+              </TouchableOpacity>
+
+              {/* <TouchableOpacity
                 onPress={() => {
                   // this.props.navigation.dispatch(
                   //   CommonActions.reset({
@@ -198,7 +283,7 @@ class MeetBefore extends React.Component<MeetBeforeProps, any> {
                   <Image source={images.google} style={GlStyles.images} />
                 </View>
                 <Text style={styles.signinTextGoogle}>Use Google Account </Text>
-              </TouchableOpacity>
+              </TouchableOpacity> */}
             </View>
           </View>
         </ScrollView>
