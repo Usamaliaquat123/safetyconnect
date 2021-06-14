eb5e8c7c5e2536f2ea00a524146afd10cad343bd
Usamaliaquat123 <inconnent12345@outlook.com>
2021-06-14 13:06:35 +0500

    clean

--- src/containers/home/SOR/Create/CreateSOR.tsx
+++ src/containers/home/SOR/Create/CreateSOR.tsx
@@ -330,14 +330,6 @@ class CreateSOR extends React.Component<CreateSORProps, any> {
         .then((user: any) => {
           var dta = user.data.data;
           this.setState({user: user.data.data});
-
-          // console.log(dta);
-          // console.log(this.state.currentOrg);
-          // console.log(
-          //   dta.organizations.filter(
-          //     (d: any) => d._id == this.state.currentOrg,
-          //   ),
-          // );
         });
     });
     this.mappingMapping(1, 1);
@@ -1280,7 +1272,7 @@ class CreateSOR extends React.Component<CreateSORProps, any> {
                 {this.state.fiveWhytoggle ? (
                   <FiveWhy
                     onChangeCountributory={(e: any) => {
-                      this.setState({countributoryCauses: e}  );
+                      this.setState({countributoryCauses: e});
                     }}
                     onChangeRiskCause={(e: any) => {
                       this.setState({rootCauses: e});
