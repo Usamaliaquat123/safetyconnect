f2c3fc3820f3edec8e2723e5c1c8a7ee3b994932
Usamaliaquat123 <inconnent12345@outlook.com>
2021-08-05 03:01:19 +0500

    save org

--- src/containers/home/CreateOrganization/CreateOrg.tsx
+++ src/containers/home/CreateOrganization/CreateOrg.tsx
@@ -14,6 +14,7 @@ import {
   imagePicker,
   cameraCapture,
   suggestInActionsRecommendations,
+  savedCurrentOrganization,
 } from '@utils';
 import RNFetchBlob from 'rn-fetch-blob';
 import {
@@ -179,6 +180,7 @@ class CreateOrg extends React.Component<CreateOrgProps, any> {
                           'invitedUsers',
                           JSON.stringify(emails),
                         );
+                        savedCurrentOrganization(res.data.data.organization_id);
                         this.props.navigation.navigate('createProject');
                       });
                   } else {
