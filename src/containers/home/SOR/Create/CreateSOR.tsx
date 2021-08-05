22ff9972bf6c37c13fddb369a24897830d9c19f8
Usamaliaquat123 <inconnent12345@outlook.com>
2021-08-05 21:02:12 +0500

    five why fix and repeated fix

--- src/containers/home/SOR/Create/CreateSOR.tsx
+++ src/containers/home/SOR/Create/CreateSOR.tsx
@@ -608,7 +608,7 @@ class CreateSOR extends React.Component<CreateSORProps, any> {
                             .getAllRepeatedSugg(
                               this.state.observationT,
                               this.state.projectid,
-                              sor.report._id,
+                              sors.report._id,
                             )
                             .then(async (sugg: any) => {
                               this.setState({
@@ -686,7 +686,7 @@ class CreateSOR extends React.Component<CreateSORProps, any> {
                         .getAllRepeatedSugg(
                           this.state.observationT,
                           this.state.projectid,
-                          sor.report._id,
+                          sors.report._id,
                         )
                         .then(async (sugg: any) => {
                           this.setState({
@@ -948,7 +948,7 @@ class CreateSOR extends React.Component<CreateSORProps, any> {
                                   .getAllRepeatedSugg(
                                     this.state.observationT,
                                     this.state.projectid, //projectid
-                                    sor.report._id,
+                                    sors.report._id,
                                   )
                                   .then(async (sugg: any) => {
                                     console.log('sugge data');
@@ -1041,7 +1041,7 @@ class CreateSOR extends React.Component<CreateSORProps, any> {
                               .getAllRepeatedSugg(
                                 this.state.observationT,
                                 this.state.projectid, //projectid
-                                sor.report._id,
+                                sors.report._id,
                               )
                               .then(async (sugg: any) => {
                                 this.setState({
