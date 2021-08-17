898d54d1eb1ea58fa72994b1137701d8d14eab75
Usamaliaquat123 <inconnent12345@outlook.com>
2021-08-17 14:48:45 +0500

    clean node

--- src/containers/home/Settings/Settings.tsx
+++ src/containers/home/Settings/Settings.tsx
@@ -346,11 +346,7 @@ class Settings extends React.Component<SettingsProps, any> {
                       <TouchableOpacity
                         style={{
                           // marginRight: wp(),
-<<<<<<< HEAD
-                          // paddingRight: wp(2),
-=======
                           paddingRight: wp(6),
->>>>>>> 2a9f65463eb87dd8a0da2534a4e70467451ba9ab
                           width: wp(10),
                           height: wp(10),
                           justifyContent: 'center',
