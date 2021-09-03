9ccfc25bd57a3d4b27b2713779f8dedf15bb1e67
Usamaliaquat123 <inconnent12345@outlook.com>
2021-09-03 15:03:28 +0500

    doen

--- android/app/src/main/java/com/safetyconnect/MainApplication.java
+++ android/app/src/main/java/com/safetyconnect/MainApplication.java
@@ -14,7 +14,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.RNFetchBlob.RNFetchBlobPackage; 
 import com.rnfs.RNFSPackage;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;  // <--- import
 
 import com.airbnb.android.react.lottie.LottiePackage;
 // import com.oblador.vectoricons.VectorIconsPackage;
@@ -33,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RNGoogleSigninPackage());
+          // packages.add(new RNFetchBlobPackage());
           return packages;
         }
 
