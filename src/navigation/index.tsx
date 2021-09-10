78dfaebaeb7a01e0a5d011da7df641b2ad5cc794
Usamaliaquat123 <inconnent12345@outlook.com>
2021-09-10 16:57:40 +0500

    cealn

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -53,7 +53,7 @@ export const Navigator = (props: any) => {
     <SafeAreaProvider>
       <NavigationContainer>
         {/* <WelcomeHome /> */}
-        <MainStackNavigator screen={'GoogleSigninOptn'} />
+        <MainStackNavigator screen={'Splash'} />
       </NavigationContainer>
     </SafeAreaProvider>
   );
