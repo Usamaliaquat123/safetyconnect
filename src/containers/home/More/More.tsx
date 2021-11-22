804b7a7770f68058a5df742d451af40f241c34ef
Usamaliaquat123 <inconnent12345@outlook.com>
2021-11-22 15:30:07 +0500

    default export

--- src/containers/home/More/More.tsx
+++ src/containers/home/More/More.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   StyleSheet,
@@ -33,201 +33,184 @@ export interface MoreProps {
   reduxActions: any;
   reduxState: any;
 }
-export default class More extends React.Component<MoreProps, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      user: {},
-      name: '',
-      email: '',
-      url: '',
-    };
-  }
 
-  componentDidMount() {
+const More = (props: MoreProps) => {
+  const [user, setuser] = useState({});
+  const [name, setname] = useState('');
+  const [email, setemail] = useState('');
+  const [url, seturl] = useState('');
+
+  useEffect(() => {
     AsyncStorage.getItem('email').then((email: any) => {
       api
         .createApi()
         .getUser(email)
         .then((user: any) => {
           var usr = user.data.data;
-          console.log(usr);
-
-          this.setState({
-            name: usr.name,
-            email: usr.email,
-            url: usr.img_url,
-            user: user.data.data,
-          });
+          setname(usr.name);
+          seturl(usr.img_url);
+          setuser(user.data.data);
+          setemail(usr.email);
         });
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <View style={{flex: 1, backgroundColor: colors.secondary}}>
-        <ScrollView>
-          <View style={styles.header}>
-            <View style={styles.headertle}>
-              <View>
-                <Text style={styles.title}>More</Text>
-                <View style={styles.underScrore} />
-              </View>
+  return (
+    <View style={{flex: 1, backgroundColor: colors.secondary}}>
+      <ScrollView>
+        <View style={styles.header}>
+          <View style={styles.headertle}>
+            <View>
+              <Text style={styles.title}>More</Text>
+              <View style={styles.underScrore} />
             </View>
           </View>
-          <View style={styles.content}>
-            <View style={styles.avatarView}>
-              <Avatar
-                size={'medium'}
-                rounded
-                source={{
-                  uri: this.state.url,
-                }}
-              />
-              <View style={{marginTop: wp(2)}}>
-                <Text style={styles.username}>{this.state.name}</Text>
-                <Text style={styles.organizations}>{this.state.email}</Text>
-              </View>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.avatarView}>
+            <Avatar
+              size={'medium'}
+              rounded
+              source={{
+                uri: url,
+              }}
+            />
+            <View style={{marginTop: wp(2)}}>
+              <Text style={styles.username}>{name}</Text>
+              <Text style={styles.organizations}>{email}</Text>
+            </View>
 
-              <TouchableOpacity
-                onPress={() => {
-                  AsyncStorage.getItem('email').then((email: any) => {
-                    api
-                      .createApi()
-                      .getUser(email)
-                      .then((user: any) => {
-                        this.props.navigation.navigate('Settings', {
-                          data: user.data.data,
-                        });
+            <TouchableOpacity
+              onPress={() => {
+                AsyncStorage.getItem('email').then((email: any) => {
+                  api
+                    .createApi()
+                    .getUser(email)
+                    .then((user: any) => {
+                      props.navigation.navigate('Settings', {
+                        data: user.data.data,
                       });
-                  });
-                }}
-                style={{position: 'absolute', right: wp(0)}}>
-                <Image style={styles.itemIcon} source={images.menuIcons.edit} />
-              </TouchableOpacity>
-            </View>
-            {/*  content */}
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('ViewAllSOr')}
-              style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.observation}
-                  />
-                </View>
-                <Text style={styles.itemText}>Observation & Feedback</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.incident}
-                  />
-                </View>
-                <Text style={styles.itemText}>
-                  Incident and Accident Reporting
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.audit}
-                  />
-                </View>
-                <Text style={styles.itemText}>Audit and Inspection</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.risk}
-                  />
-                </View>
-                <Text style={styles.itemText}>Risk Management</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.lms}
-                  />
-                </View>
-                <Text style={styles.itemText}>LMS</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.data}
-                  />
-                </View>
-                <Text style={styles.itemText}>Data Analytics</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('MainSettings')}
-              style={styles.containerOfItem}>
-              <View style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.settings}
-                  />
-                </View>
-                <Text style={styles.itemText}>Settings</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.containerOfItem, {marginBottom: wp(15)}]}>
-              <TouchableOpacity
-                onPress={() => {
-                  Auth.signOut();
-
-                  AsyncStorage.removeItem('user');
-                  AsyncStorage.removeItem('email');
-                  AsyncStorage.removeItem('organizationId');
-                  AsyncStorage.removeItem('projectId');
-                  GoogleSignin.signOut();
-                  this.props.navigation.dispatch(
-                    CommonActions.reset({
-                      index: 1,
-                      routes: [
-                        {
-                          name: 'Login',
-                        },
-                      ],
-                    }),
-                  );
-                }}
-                style={{flexDirection: 'row'}}>
-                <View>
-                  <Image
-                    style={styles.itemIcon}
-                    source={images.menuIcons.signout}
-                  />
-                </View>
-                <Text style={styles.itemText}>Sign out</Text>
-              </TouchableOpacity>
+                    });
+                });
+              }}
+              style={{position: 'absolute', right: wp(0)}}>
+              <Image style={styles.itemIcon} source={images.menuIcons.edit} />
             </TouchableOpacity>
           </View>
-        </ScrollView>
-      </View>
-    );
-  }
-}
+          {/*  content */}
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate('ViewAllSOr')}
+            style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image
+                  style={styles.itemIcon}
+                  source={images.menuIcons.observation}
+                />
+              </View>
+              <Text style={styles.itemText}>Observation & Feedback</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image
+                  style={styles.itemIcon}
+                  source={images.menuIcons.incident}
+                />
+              </View>
+              <Text style={styles.itemText}>
+                Incident and Accident Reporting
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image
+                  style={styles.itemIcon}
+                  source={images.menuIcons.audit}
+                />
+              </View>
+              <Text style={styles.itemText}>Audit and Inspection</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image style={styles.itemIcon} source={images.menuIcons.risk} />
+              </View>
+              <Text style={styles.itemText}>Risk Management</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image style={styles.itemIcon} source={images.menuIcons.lms} />
+              </View>
+              <Text style={styles.itemText}>LMS</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image style={styles.itemIcon} source={images.menuIcons.data} />
+              </View>
+              <Text style={styles.itemText}>Data Analytics</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate('MainSettings')}
+            style={styles.containerOfItem}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Image
+                  style={styles.itemIcon}
+                  source={images.menuIcons.settings}
+                />
+              </View>
+              <Text style={styles.itemText}>Settings</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.containerOfItem, {marginBottom: wp(15)}]}>
+            <TouchableOpacity
+              onPress={() => {
+                Auth.signOut();
+
+                AsyncStorage.removeItem('user');
+                AsyncStorage.removeItem('email');
+                AsyncStorage.removeItem('organizationId');
+                AsyncStorage.removeItem('projectId');
+                GoogleSignin.signOut();
+                props.navigation.dispatch(
+                  CommonActions.reset({
+                    index: 1,
+                    routes: [
+                      {
+                        name: 'Login',
+                      },
+                    ],
+                  }),
+                );
+              }}
+              style={{flexDirection: 'row'}}>
+              <View>
+                <Image
+                  style={styles.itemIcon}
+                  source={images.menuIcons.signout}
+                />
+              </View>
+              <Text style={styles.itemText}>Sign out</Text>
+            </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default More;
 
 // const mapStateToProps = state => {
 //   return {
